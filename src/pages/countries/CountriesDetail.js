236a276e8b608e730e92d223e5d0dda209ee5729
Vincent Leclerc <vleclerc@inow.fr>
2023-09-27 11:07:00 +0200

    routes params

--- src/pages/countries/CountriesDetail.js
+++ src/pages/countries/CountriesDetail.js
@@ -1,8 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
 
 const CountriesDetail = () => {
+    const { name } = useParams();
+    const [country, setCountry] = useState({});
+
+    useEffect(() => {
+        axios.get(`https://restcountries.com/v3.1/name/${name}`).then(resp => {
+            setCountry(resp.data[0]);
+        }).catch(er => {
+            alert(er.message);
+        });
+
+    }, [])
+
     return (
         <div>
-            <h1></h1>
+            <h1>{name}</h1>
+            <ul>
+                {country?.capital?.map((c) => (
+                    <li key={c}>{c}</li>
+                ))}
+            </ul>
         </div>
     );
 }
